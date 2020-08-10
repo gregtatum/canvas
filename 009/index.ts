d63672b16878113144d2c0ec7e64a4cd0c7f73ac
Greg Tatum <tatum.creative@gmail.com>
2020-08-10 14:26:40 -0500

    Make the point clumping interesting

--- 009/index.ts
+++ 009/index.ts
@@ -18,11 +18,18 @@ function getConfig() {
     ctx,
     seed,
     random,
+    margin: 0.25,
     baseSphereSpeed: 30,
     baseSphereAngularVelocity: 0.1,
+    /** Determines the Math.pow value to distribute sphere sizes */
+    gravity: 3,
+    sphereDistributionPower: 5,
+    keepInBoundsImpulse: 2,
     ticksPerSecond: 60,
     sphereCount: 500,
-    sphereSizes: [2, 100],
+    restitution: 0.7,
+    /** Create spheres sized according to a ratio of the diagonal of the screen */
+    sphereSizes: [0.001, 0.08],
   };
 }
 
@@ -30,6 +37,7 @@ function getConfig() {
 function getCurrent(config: Config) {
   const world = Physics.create.world({
     ticksPerSecond: config.ticksPerSecond,
+    gravity: Physics.vec2.create(0, config.gravity),
   });
 
   return {
@@ -37,14 +45,30 @@ function getCurrent(config: Config) {
     dt: 0,
     tick: 0,
     world,
+    mouseX: null as number | null,
+    mouseY: null as number | null,
     spheres: createSpheres(config, world),
   };
+}
+
+function setupMouseHandlers(current: Current): void {
+  window.addEventListener("touchmove", event => {
+    const { pageX, pageY } = event.touches[0];
+    current.mouseX = pageX;
+    current.mouseY = pageY;
+  });
+
+  window.addEventListener("mousemove", event => {
+    current.mouseX = event.pageX;
+    current.mouseY = event.pasgeY;
+  });
 }
 
 {
   const config = getConfig();
   const current = getCurrent(config);
   (window as any).current = current;
+  setupMouseHandlers(current);
 
   loop((time, dt) => {
     current.dt = dt;
@@ -53,24 +77,28 @@ function getCurrent(config: Config) {
 
     current.world.integrate(dt);
 
+    const { margin, keepInBoundsImpulse } = config;
+    const { mouseX, mouseY } = current;
+    const forceX = 0.3 * (mouseX ? mouseX - innerWidth / 2 : 0);
+    const forceY = 0.3 * (mouseY ? mouseY - innerHeight / 2 : 0);
+    const left = forceX + innerWidth * margin;
+    const right = forceX + innerWidth * (1 - margin);
+    const bottom = forceY + innerHeight * (1 - margin);
+    const top = forceY + innerHeight * margin;
+
     for (const { body } of current.spheres) {
-      const keepInBoundsForce = 0.1;
-      if (body.position.x < 0) {
-        Physics.vec2.reflect(body.velocity, Physics.vec2.create(1, 0));
-        body.velocity.x += keepInBoundsForce;
-      } else if (body.position.y < 0) {
-        Physics.vec2.reflect(body.velocity, Physics.vec2.create(0, 1));
-        body.velocity.y += keepInBoundsForce;
-      } else if (body.position.y > innerHeight) {
-        Physics.vec2.reflect(body.velocity, Physics.vec2.create(0, -1));
-        body.velocity.y -= keepInBoundsForce;
-      } else if (body.position.x > innerWidth) {
-        Physics.vec2.reflect(body.velocity, Physics.vec2.create(-1, 0));
-        body.velocity.x -= keepInBoundsForce;
-      }
-      // Add some energy to the system
-      if (Math.random() < 0.5) {
-        Physics.vec2.multiplyScalar(body.velocity, 2);
+      if (body.position.x < left) {
+        // Physics.vec2.reflect(body.velocity, Physics.vec2.create(1, 0));
+        body.velocity.x += keepInBoundsImpulse;
+      } else if (body.position.y < top) {
+        // Physics.vec2.reflect(body.velocity, Physics.vec2.create(0, 1));
+        body.velocity.y += keepInBoundsImpulse;
+      } else if (body.position.y > bottom) {
+        // Physics.vec2.reflect(body.velocity, Physics.vec2.create(0, -1));
+        body.velocity.y -= keepInBoundsImpulse;
+      } else if (body.position.x > right) {
+        // Physics.vec2.reflect(body.velocity, Physics.vec2.create(-1, 0));
+        body.velocity.x -= keepInBoundsImpulse;
       }
     }
 
@@ -89,31 +117,38 @@ function createSpheres(
     sphereSizes,
     baseSphereSpeed,
     baseSphereAngularVelocity,
+    sphereDistributionPower,
+    margin,
+    restitution,
   } = config;
   const spheres: Set<Physics.Sphere> = new Set();
   for (let i = 0; i < sphereCount; i++) {
     let sphere, intersection;
+    const screenDiameter = Math.sqrt(
+      innerWidth * innerWidth + innerHeight * innerHeight
+    );
+    const min = sphereSizes[0] * screenDiameter;
+    const max = sphereSizes[1] * screenDiameter;
 
     do {
       const position = {
-        x: random(innerWidth),
-        y: random(innerHeight),
+        x: random(innerWidth * margin, innerWidth * (1 - margin)),
+        y: random(innerHeight * margin, innerHeight * (1 - margin)),
       };
-      const [min, max] = sphereSizes;
-      const radius = Math.pow(random(), 4) * (max - min) + min;
+
+      const radius =
+        Math.pow(random(), sphereDistributionPower) * (max - min) + min;
       sphere = Physics.create.sphere(position, radius);
       sphere.body.mass = Math.PI * radius * radius;
       intersection = Physics.findSingleIntersection(sphere, spheres);
     } while (intersection);
     const { body } = sphere;
 
-    body.fixedPosition = true;
-    body.friction = 0.9;
     body.velocity.x = random(-baseSphereSpeed, baseSphereSpeed);
     body.velocity.y = random(-baseSphereSpeed, baseSphereSpeed);
     body.rotation = random(-Math.PI, Math.PI);
     body.angularVelocity = random(baseSphereAngularVelocity);
-    body.restitution = 1;
+    body.restitution = restitution;
     spheres.add(sphere);
     world.addToAllGroup(sphere);
   }
@@ -127,9 +162,9 @@ function clearScreen(config: Config): void {
 }
 
 function drawWorld(config: Config, current: Current): void {
-  const { world } = current;
+  const { world, time } = current;
   const { ctx } = config;
-  ctx.strokeStyle = `hsl(100, 100%, 50%)`;
+  ctx.strokeStyle = `hsl(${60 + time * 10}, 100%, 50%)`;
   ctx.beginPath();
   ctx.lineWidth = 2;
   ctx.lineCap = "square";
